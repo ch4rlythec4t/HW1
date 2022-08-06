a897378f8f113b1b85838ffbcbd79f9a8d9bba6c
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-06 10:25:18 +0300

    Task8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вводим  число
+Console.WriteLine("Введите целое число");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= Num; i++)
+{
+    if ((i % 2) == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
