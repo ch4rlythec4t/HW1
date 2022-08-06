783f513947d9d38dc7a7cb58635708032f5b1a7a
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-06 10:10:59 +0300

    Task4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вводим 1-е число
+Console.WriteLine("Введите 1-е число");
+double Num1 = Convert.ToDouble(Console.ReadLine());
+
+// Вводим 2-е число
+Console.WriteLine("Введите 2-е число");
+double Num2 = Convert.ToDouble(Console.ReadLine());
+
+// Вводим 3-е число
+Console.WriteLine("Введите 3-е число");
+double Num3 = Convert.ToDouble(Console.ReadLine());
+
+// Вводим понятия максимального числа
+double Max = Num1;
+
+if (Num2 > Max)
+{
+    Max = Num2;
+}
+
+if (Num3 > Max)
+{
+    Max = Num3;
+}
+
+Console.WriteLine($"Макcимальным числом является {Max}");
